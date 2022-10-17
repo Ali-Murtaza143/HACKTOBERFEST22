711e832e73458bd5dc99acd42355f5b08e0e2e48
Shahzain Ahmed <59369881+ShahzainAhmed@users.noreply.github.com>
2022-10-17 13:19:09 +0500

    modified the code to give a desired output

--- DAY1(1 HOUR JS)/changing the style tag with JS/main.js
+++ DAY1(1 HOUR JS)/changing the style tag with JS/main.js
@@ -1,10 +1,10 @@
-var num=4;
+var num=86;
 
 
 function myFunc1()
 {
-   document.getElementById('paragrapgh').style.color='red';
+   document.getElementById('paragrapgh').style.color='yellow';
   
    var VText=document.getElementById('ChangeText').value;
-   document.getElementById('paragrapgh').innerHTML=VText;
+   document.getElementById('paragraph').innerHTML=VText;
    }
