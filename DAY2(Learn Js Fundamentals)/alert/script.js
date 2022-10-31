f928521c99fcfdf90a6fc3b32fefade0e86dbadf
Shahzaib Memon <57555093+Shahzaib-D-Memon@users.noreply.github.com>
2022-10-31 21:33:08 +0500

    Update script.js
    Updating file

--- DAY2(Learn Js Fundamentals)/alert/script.js
+++ DAY2(Learn Js Fundamentals)/alert/script.js
@@ -1,4 +1,11 @@
-alert('hello world, This is a JS alet box');
+alert('hello world, This is a JS alert box');
+const lang = "Japanese";
+const universe = 101;
+const user = "Shanks";
+console.log(lang);
+console.log(universe);
+console.log(user);
+
 
 //print hello world
 
