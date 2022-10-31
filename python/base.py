9ea66febf39247e3513564d2924476e216603b2d
Haris <56627510+MuhammadHarisKhalil@users.noreply.github.com>
2022-10-31 22:24:34 +0500

    Changed the Iterator to initiate it from 0.

--- python/base.py
+++ python/base.py
@@ -1,7 +1,7 @@
 h = int(input("Hypotenuse: "))
 trigger = False
-for i in range(1, h+1):
-    for j in range(1, h+1):
+for i in range(0, h+1):
+    for j in range(0, h+1):
         if h**2 == i**2 + j**2:
             print(f"{h}^2 = {i}^2 + {j}^2")
             print(f"{h**2} = {i**2} + {j**2} = {i**2 + j**2}")
